65919e5c587d96e95f202925421c6492424a25cf
susil <thapasusil53@gmail.com>
2020-07-24 08:39:27 +0545

    debug set to False

--- first_blog_project/settings.py
+++ first_blog_project/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = ')bgaoast05bff&-nc9$blf4rbyf+8=7hpt*dsk6ozw99@1fcje'
 #os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # blogapp007.herokuapp.com
 ALLOWED_HOSTS = ['*']
