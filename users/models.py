c23cabed37fe18b3e91ee4099eae6c3899c321a4
susil <thapasusil53@gmail.com>
2020-08-29 14:20:50 +0545

    image_url field in Profile model to store users image url

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ from django.shortcuts import reverse
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image_url = models.URLField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
